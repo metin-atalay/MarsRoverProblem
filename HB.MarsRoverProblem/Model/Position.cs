5e64bb63455c76c46a2856b5c3a22d9d92b90bab
metin.atalay <metin.atalay@itsfindev.com>
2021-01-10 15:13:58 +0300

    Add project files.

---
+++ HB.MarsRoverProblem/Model/Position.cs
@@ -0,0 +1,16 @@
+﻿using HB.MarsRoverProblem.Interface;
+
+namespace HB.MarsRoverProblem.Model
+{
+    public class Position : IPosition
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Position(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
